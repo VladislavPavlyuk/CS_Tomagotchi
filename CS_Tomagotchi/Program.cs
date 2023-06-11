857fb947d63e31d76b0c7f658566be5cefafcb00
vladp_d3fqm45 <vladp_d3fqm45@DESKTOP-FQLK9B1>
2023-06-11 18:15:47 +0200

    Add project files.

---
+++ CS_Tomagotchi/Program.cs
@@ -0,0 +1,114 @@
+﻿/*
+Разработать приложение «Тамагочи». Жизненный цикл персонажа — 1-2 минуты. 
+Персонаж случайным образом выдаёт просьбы (но подряд одна и та же просьба не выдаётся). 
+Просьбы могут быть следующие: 
+- покормить, 
+- погулять, 
+- уложить спать, 
+- полечить, 
+- поиграть. 
+Если просьбы не удовлетворяются трижды, персонаж «заболевает» и просит его полечить. В случае отказа — «умирает». Персонаж отображается в консольном окне при помощи псевдографики.
+ */
+
+using CS_Tomagotchi;
+using System;
+using System.Timers;
+
+namespace Delegate
+{
+
+    public delegate void TomagotchiStateHandler(string m);
+
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            System.Timers.Timer timer = new System.Timers.Timer();
+
+            timer.Interval = 1000;
+
+            Tomagotchi t = new Tomagotchi(100, 60);
+
+            // public event ElapsedEventHandler Elapsed - это событие происходит по истечении интервала времени
+            timer.Elapsed += new ElapsedEventHandler(t.OnTimer);
+
+            timer.Start(); // Начинает вызывать событие Elapsed            
+
+            t.feedMe += Green_Message;
+            t.walkMe += Green_Message;
+            t.sleepMe += Green_Message;
+            t.playMe += Green_Message;
+            t.treatMe += Yellow_Message;
+            t.dead += Red_Message;
+
+            Random rnd = new Random();
+
+            int e = rnd.Next(1, 4);   
+                
+            int temp = e;
+
+            while (true)
+            {
+
+                Console.WriteLine("\n Энергия : {0} Отказы : {1}", t.CurrentPower, t.Refusal);
+
+
+                while (e == temp)        //контроль повторения событий
+                {
+                    e = rnd.Next(1, 5);
+                }
+
+                temp = e;
+
+                switch (e)
+                {
+                    case 1: 
+                        t.FeedMe();
+                        break;
+                    case 2:
+                        t.WalkMe();
+                        break; ;
+                    case 3:
+                        t.SleepMe();
+                        break;
+                    case 4:
+                        t.PlayMe();
+                        break;
+
+                    default: 
+                        break;
+                }
+                if (t.Refusal >= 3) t.TreatMe();
+
+                if (!t.Alive()) t.Die();
+
+                Console.Clear();
+            }
+
+        }
+
+
+
+        private static void Green_Message(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+        private static void Yellow_Message(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+        private static void Red_Message(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
+
---
+++ CS_Tomagotchi/Tomagotchi.cs
@@ -0,0 +1,256 @@
+﻿
+using Delegate;
+using System.Timers;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CS_Tomagotchi
+{
+
+    class Tomagotchi
+    {
+        int  _power, _refusal, _count;
+
+        public Tomagotchi(int power, int count) 
+        { 
+            _power = power;
+            _count = count;
+        }
+
+        public int Count
+        {
+            set => _count = value;
+
+            get => _count;
+        }
+        public int CurrentPower
+        {
+            set => _power = value;
+
+            get => _power;
+        }
+
+        public int Refusal
+        {
+            set => _refusal = value;
+
+            get => _refusal;
+        }
+
+        public bool Act()
+        {
+            ConsoleKeyInfo a;            
+
+            while (true || false)
+            {
+                Console.Write("\n Удовлетворить просьбу? (Y/N) :");
+
+                a = Console.ReadKey();
+
+                if (a.Key == ConsoleKey.Y)
+                    return true;
+ 
+                if (a.Key == ConsoleKey.N)
+                    return false;
+                else
+                    Console.Write("\n Ошибка! Попробуйте еще раз...");
+            }
+        }
+
+        public void FeedMe()
+        {
+            CurrentPower -= 10;
+
+            PrintFunny();
+            feedMe?.Invoke("\n Я голодный! Покорми меня! ");
+
+            if (Act())
+            {
+                CurrentPower += 25;
+                Refusal = 0;
+            }
+            else
+            {
+                Refusal++;
+                Console.Clear();
+                Console.WriteLine("\n");
+                PrintSad();
+                Console.ReadKey();
+            }
+        }
+
+        public void WalkMe()
+        {
+            CurrentPower -= 10;
+
+            PrintFunny();
+            feedMe?.Invoke("\n Я устал! Погуляй со мной! ");
+
+            if (Act())
+            {
+                CurrentPower += 25;
+                Refusal = 0;
+            }
+            else
+            {
+                Refusal++;
+                Console.Clear();
+                Console.WriteLine("\n");
+                PrintSad();
+                Console.ReadKey();
+            }
+        }
+
+        public void SleepMe()
+        {
+            CurrentPower -= 10;
+
+            PrintFunny();
+            feedMe?.Invoke("\n Я устал! Уложи меня спать! ");
+
+            if (Act())
+            {
+                CurrentPower += 25;
+                Refusal = 0;
+            }
+            else
+            {
+                Refusal++;
+                Console.Clear();
+                Console.WriteLine("\n");
+                PrintSad();
+                Console.ReadKey();
+            }
+        }
+
+        public void PlayMe()
+        {
+            CurrentPower -= 10;
+
+            PrintFunny();
+            playMe?.Invoke("\n Мне скучно! Поиграй со мной! ");
+
+            if (Act())
+            {
+                CurrentPower += 25;
+                Refusal = 0;
+            }
+            else
+                {
+                    Refusal++;
+                    Console.Clear();
+                    Console.WriteLine("\n");
+                    PrintSad();
+                    Console.ReadKey();
+                }
+            }
+
+        public void TreatMe()
+        {
+            CurrentPower -= 10;
+                        
+            treatMe?.Invoke("\n Я заболел! Полечи меня! ");
+
+            if (Act())
+            {
+                CurrentPower += 25;
+                Refusal = 0;
+            }
+            else Die();
+        }
+
+        public void Die()
+        {
+            Console.Clear();
+            Console.WriteLine("\n");
+            PrintDead();
+
+            dead?.Invoke("\n Я умер! Энергия: " + CurrentPower + " Отказы: " + Refusal + " Время жизни: " + Count);
+
+            Environment.Exit(0);
+        }
+
+        public bool Alive()
+        {
+            if (CurrentPower <= 0) return false;
+            else            
+                return true;
+        }
+
+        public void PrintFunny()
+        {
+            Console.WriteLine();
+            Console.WriteLine("     **        **");
+            Console.WriteLine("    ****      ****");
+            Console.WriteLine("    *****    *****");
+            Console.WriteLine("     ************");
+            Console.WriteLine("     *          * ");
+            Console.WriteLine("    *   (.)  (.) *  ");
+            Console.WriteLine("    *            *");
+            Console.WriteLine("  ****   *   *  ****");
+            Console.WriteLine("  ****    ***   ****   ");
+            Console.WriteLine("      **********   ");
+            Console.WriteLine("       ***   ***   ");
+            Console.WriteLine("\n\n Нажмите любую клавишу...");
+        }
+
+        public void PrintSad()
+        {
+            Console.WriteLine();
+            Console.WriteLine("     **        **");
+            Console.WriteLine("    ****      ****");
+            Console.WriteLine("    *****    *****");
+            Console.WriteLine("     ************");
+            Console.WriteLine("     *          * ");
+            Console.WriteLine("    *   (.)  (.) *  ");
+            Console.WriteLine("    *            *");
+            Console.WriteLine("  ****    ***   ****");
+            Console.WriteLine("  ****   *   *  ****   ");
+            Console.WriteLine("      **********   ");
+            Console.WriteLine("       ***   ***   ");
+            Console.WriteLine("\n\n Нажмите любую клавишу...");
+        }
+
+        public void PrintDead()
+        {
+            Console.WriteLine();
+            Console.WriteLine("     **        **");
+            Console.WriteLine("    ****      ****");
+            Console.WriteLine("    *****    *****");
+            Console.WriteLine("     ************");
+            Console.WriteLine("     *          * ");
+            Console.WriteLine("    *    x    x  *  ");
+            Console.WriteLine("    *            *");
+            Console.WriteLine("  ****    ***   ****");
+            Console.WriteLine("  ****          ****   ");
+            Console.WriteLine("      **********   ");
+            Console.WriteLine("       ***   ***   ");
+        }
+
+        //static int count = 0;
+
+        public void OnTimer(object sender, ElapsedEventArgs arg /* Предоставляет данные для события Elapsed */)
+        {
+            System.Timers.Timer timer = (System.Timers.Timer)sender;
+
+            Count--;
+
+            if (Count == 0)
+            {
+                timer.Stop();
+
+                Die();
+            }
+
+            Console.Title = String.Format(" Томагочи. Время жизни {0} секунд.", Count);
+        }
+
+        public event TomagotchiStateHandler feedMe;
+        public event TomagotchiStateHandler walkMe;
+        public event TomagotchiStateHandler sleepMe;
+        public event TomagotchiStateHandler treatMe;
+        public event TomagotchiStateHandler playMe;
+
+        public event TomagotchiStateHandler dead;
+    }
+}
+
